19163648af314c25984a0aa06c35e3c38a780123
RaphaelIT7 <64648134+RaphaelIT7@users.noreply.github.com>
2025-04-07 07:54:57 +0200

    Throw an error

---
+++ lua/tests/gluatest/Test1.lua
@@ -0,0 +1,11 @@
+return {
+    groupName = "Test1",
+    cases = {
+        {
+            name = "Error",
+            func = function()
+                return error("TestError")
+            end
+        },
+    }
+}
